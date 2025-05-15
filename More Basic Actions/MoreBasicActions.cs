f9eb18d6eb3e004924f49e2b81900952b994af14
Anase Skyrider <anaseskyrider@gmail.com>
2025-05-15 05:57:22 -0400

    Ready (not actually ready)
    Barely preparing for Ready action.

--- More Basic Actions/MoreBasicActions.cs
+++ More Basic Actions/MoreBasicActions.cs
@@ -7,7 +7,7 @@ public class MoreBasicActions
     [DawnsburyDaysModMainMethod]
     public static void LoadMod()
     {
-        //Ready.LoadMod(); // TODO: "Readying an attack is useful under two circumstances. First, an enemy comes within reach/range. This would be easy to implement. Second, an enemy is made flat-footed or flanked (most relevant for rogues)." - Dinglebob
         Aid.LoadAid();
+        Ready.LoadReady();
     }
 }
---
+++ More Basic Actions/Ready.cs
@@ -0,0 +1,9 @@
+namespace Dawnsbury.Mods.MoreBasicActions;
+
+public static class Ready
+{
+    public static void LoadReady()
+    {
+        // TODO: "Readying an attack is useful under two circumstances. First, an enemy comes within reach/range. This would be easy to implement. Second, an enemy is made flat-footed or flanked (most relevant for rogues)." - Dinglebob
+    }
+}
