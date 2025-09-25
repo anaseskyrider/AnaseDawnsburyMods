eebe2abd29801cd3e30f3fe2e0608d1ff8bf2d2a
Anase Skyrider <anaseskyrider@gmail.com>
2025-09-24 23:41:54 -0400

    Alphabetize

--- Guardian Class/ModData.cs
+++ Guardian Class/ModData.cs
@@ -24,44 +24,10 @@ public static class ModData
             true);*/
     }
 
-    public static class Tooltips
+    public static class ActionIds
     {
-        public static readonly Func<string, string> CommonWeaponSpec = RegisterTooltipInserter(
-            "Guardian.Common.WeaponSpecialization",
-            "{b}Weapon Specialization{/b}\n{i}Common class feature{/i}\nYou deal 2 additional damage with weapons and unarmed attacks in which you are an expert; this damage increases to 3 if you're a master, and to 4 if you're legendary.");
-        public static readonly Func<string, string> CommonGreaterWeaponSpec = RegisterTooltipInserter(
-            "Guardian.Common.GreaterWeaponSpecialization",
-            "{b}Greater Weapon Specialization{/b}\n{i}Common class feature{/i}\nYour damage from weapon specialization increases to 4 with weapons and unarmed attacks in which you're expert, 6 if you're a master, and 8 if you're legendary.");
-        public static readonly Func<string, string> ActionTaunt = RegisterTooltipInserter(
-            "Guardian.Actions.Taunt",
-            "{b}Taunt{/b} {icon:Action}\n{i}Concentrate, (Visual or Auditory){/i}\nChoose an enemy within 30 feet to be your taunted enemy. If your taunted enemy takes a hostile action that includes at least one of your allies but doesn't include you, they take a –1 circumstance penalty to their attack rolls and DCs for that action, and they also become off-guard until the start of their next turn.\n\nYour enemy remains taunted until the start of your next turn, and you can have only one Taunt in effect at a time. Taunting a new enemy ends this effect on any current target.\n\nTaunt gains the auditory trait, visual trait, or both, depending on how you draw the target's attention.");
-        public static readonly Func<string, string> ActionInterceptAttack = RegisterTooltipInserter(
-            "Guardian.Actions.InterceptAttack",
-            "{b}Intercept Attack{/b} {icon:Reaction}\n{b}Trigger{/b} An ally within 10 feet of you takes physical damage.\n\nYou can Step, but you must end your movement adjacent to the triggering ally. You take the damage instead of the triggering ally. Apply your own immunities, weaknesses, and resistances to the damage, not the ally's.\n\n{b}Special{/b} You can extend this ability to an ally within 15 feet of you if the damage comes from your taunted enemy. If this ally is farther than you can Step to reach, you can Stride instead of Stepping; you still must end the movement adjacent to your ally.");
-        public static readonly Func<string, string> ArmorResting = RegisterTooltipInserter(
-            "Guardian.Feature.GuardiansArmorResting",
-            "{b}Resting in Armor{/b}\n{i}Common rule{/i}\nSleeping in armor is uncomfortable, and would lead to poor-quality sleep. Some encounters occur while the party is sleeping. If you aren't able to sleep in armor, you won't have your armor donned at the start of combat.");
-        public static readonly Func<string, string> FeatureToughToKill = RegisterTooltipInserter(
-            "Guardian.Feature.ToughToKill",
-            "{b}Tough to Kill{/b}\n{i}Level 3 Guardian feature{/i}\nYou gain the Diehard general feat {i}(you should retrain it if you already have it){/i}. Additionally, the first time each day you'd be reduced to dying 3 or higher, you stay at dying 2 instead.");
-        public static readonly Func<string, string> FeatureReactionTime = RegisterTooltipInserter(
-            "Guardian.Feature.ReactionTime",
-            "{b}Reaction Time{/b}\n{i}Level 7 Guardian feature{/i}\nAt the start of combat and for each of your turns, you gain an additional reaction that you can use only for reactions from guardian feats or class features (including Shield Block).");
-        public static readonly Func<string, string> FeatureBattleHardened = RegisterTooltipInserter(
-            "Guardian.Feature.BattleHardened",
-            "{b}Battle Hardened{/b}\n{i}Level 9 Guardian feature{/i}\nYour proficiency rank for Fortitude saves increases to master; when you roll a success on a Fortitude save, you get a critical success instead.");
-        public static readonly Func<string, string> FeatureUnyieldingResolve = RegisterTooltipInserter(
-            "Guardian.Feature.UnyieldingResolve",
-            "{b}Unyielding Resolve{/b}\n{i}Level 17 Guardian feature{/i}\nYour proficiency rank for Will saves increases to master; when you roll a success on a Will save, you get a critical success instead.");
-        public static readonly Func<string, string> FeatureGuardianMastery = RegisterTooltipInserter(
-            "Guardian.Feature.GuardianMastery",
-            "{b}Guardian Mastery{/b}\n{i}Level 19 Guardian feature{/i}\nWhile wearing armor, when you attempt a Reflex save, you can add your armor's item bonus to AC instead of your Dexterity modifier if it's higher; if your armor has the bulwark trait, increase this bonus by 1. If you get a success when you do this, you get a critical success instead.");
-        
-        public static Func<string, string> RegisterTooltipInserter(string tooltipName, string tooltipDescription)
-        {
-            ModManager.RegisterInlineTooltip(tooltipName, tooltipDescription);
-            return input => "{tooltip:" + tooltipName + "}" + input + "{/}";
-        }
+        public static readonly ActionId Taunt = ModManager.RegisterEnumMember<ActionId>("Taunt");
+        public static readonly ActionId InterceptAttack = ModManager.RegisterEnumMember<ActionId>("InterceptAttack");
     }
     
     public static class BooleanOptions
@@ -72,38 +38,44 @@ public static class ModData
         
         //public const string UnrestrictedTrace = "RunesmithPlaytest.UnrestrictedTrace";
     }
-    
-    public static class PossibilityGroups
+
+    public static class CommonQfKeys
     {
-        public const string TauntActions = "Taunt Actions";
+        /// <summary>This key includes the name of the Guardian who inflicted it -- search using this name + Creature.Name. Searching for the source of this effect also ensures that the creature is off-guard due to ignoring YOUR Taunt.</summary>
+        public static readonly string OffGuardDueToTaunt = "TauntOffGuard";
     }
 
-    public static class PersistentActions
+    public static class CommonRequirements
     {
-        public const string ToughToKill = "ToughToKill";
-    }
-    
-    public static class Traits
-    {
-        #region Class
-        public static readonly Trait Guardian = ModManager.RegisterTrait("Guardian", 
-            new TraitProperties("Guardian", true) { IsClassTrait = true });
-        #endregion
-    
-        #region Features
-        
-        #endregion
-    
-        #region Feats
-        public static readonly Trait BodyguardCharge = ModManager.RegisterTrait("Bodyguard's Charge",
-            new TraitProperties("Bodyguard's Charge", false));
-        #endregion
+        public static CreatureTargetingRequirement MustWearMediumOrHeavyArmor()
+        {
+            return new LegacyCreatureTargetingRequirement((a,_) =>
+                IsWearingMediumOrHeavyArmor(a)
+                    ? Usability.Usable
+                    : Usability.NotUsable("must be wearing medium or heavy armor"));
+        }
+
+        public static bool IsWearingMediumOrHeavyArmor(Creature cr)
+        {
+            return (cr.Armor.Item is {} armor1
+                    && (armor1.HasTrait(Trait.MediumArmor) || armor1.HasTrait(Trait.HeavyArmor)))
+                   || (cr.BaseArmor is {} armor2
+                       && (armor2.HasTrait(Trait.MediumArmor) || armor2.HasTrait(Trait.HeavyArmor)));
+        }
     }
     
     public static class FeatNames
     {
         #region Class
         public static readonly FeatName GuardianClass = ModManager.RegisterFeatName("GuardianClass.GuardianClass", "Guardian");
+        #endregion
+
+        #region Class Archetype
+        public static FeatName GuardianDedication; // Data filled in GuardianArchetype.LoadArchetype().
+        public static readonly FeatName GuardiansIntercept = ModManager.RegisterFeatName("GuardianClass.GuardiansIntercept", "Guardian's Intercept");
+        public static readonly FeatName ArmoredResistance = ModManager.RegisterFeatName("GuardianClass.ArmoredResistance", "Armored Resistance");
+        public static readonly FeatName IroncladFortitude = ModManager.RegisterFeatName("GuardianClass.IroncladFortitude", "Ironclad Fortitude");
+
         #endregion
         
         #region Class Features
@@ -155,24 +127,6 @@ public static class ModData
         public static readonly FeatName ShieldWallop = ModManager.RegisterFeatName("GuardianClass.ShieldWallop", "Shield Wallop");
         #endregion
     }
-    
-    public static class QEffectIds
-    {
-        public static readonly QEffectId TauntTarget = ModManager.RegisterEnumMember<QEffectId>("TauntTarget");
-        public static readonly QEffectId BodyguardCharge = ModManager.RegisterEnumMember<QEffectId>("Bodyguard's Charge");
-    }
-
-    public static class CommonQFKeys
-    {
-        /// <summary>This key includes the name of the Guardian who inflicted it -- search using this name + Creature.Name. Searching for the source of this effect also ensures that the creature is off-guard due to ignoring YOUR Taunt.</summary>
-        public static readonly string OffGuardDueToTaunt = "TauntOffGuard";
-    }
-
-    public static class ActionIds
-    {
-        public static readonly ActionId Taunt = ModManager.RegisterEnumMember<ActionId>("Taunt");
-        public static readonly ActionId InterceptAttack = ModManager.RegisterEnumMember<ActionId>("InterceptAttack");
-    }
 
     public static class Illustrations
     {
@@ -181,6 +135,27 @@ public static class ModData
         public static readonly Illustration ArmoredCourage = new ModdedIllustration("GuardianClassAssets/armor-upgrade 2.png");
         public static readonly Illustration StompGround = new ModdedIllustration("GuardianClassAssets/quake-stomp.png");
         public static readonly string DawnsburySunPath = "GuardianClassAssets/PatreonSunTransparent.png";
+    }
+
+    public static class PersistentActions
+    {
+        public const string ToughToKill = "ToughToKill";
+    }
+    
+    public static class PossibilityGroups
+    {
+        public const string TauntActions = "Taunt Actions";
+    }
+    
+    public static class PossibilitySectionIds
+    {
+        public static readonly PossibilitySectionId BasicTaunts = ModManager.RegisterEnumMember<PossibilitySectionId>("BasicTaunts");
+    }
+    
+    public static class QEffectIds
+    {
+        public static readonly QEffectId TauntTarget = ModManager.RegisterEnumMember<QEffectId>("TauntTarget");
+        public static readonly QEffectId BodyguardCharge = ModManager.RegisterEnumMember<QEffectId>("Bodyguard's Charge");
     }
     
     public static class SfxNames
@@ -199,19 +174,60 @@ public static class ModData
         public static readonly SubmenuId Taunt = ModManager.RegisterEnumMember<SubmenuId>("Taunt");
     }
     
-    public static class PossibilitySectionIds
+    public static class Traits
     {
-        public static readonly PossibilitySectionId BasicTaunts = ModManager.RegisterEnumMember<PossibilitySectionId>("BasicTaunts");
+        #region Class
+        public static readonly Trait Guardian = ModManager.RegisterTrait("Guardian", 
+            new TraitProperties("Guardian", true) { IsClassTrait = true });
+        #endregion
+    
+        #region Features
+        
+        #endregion
+    
+        #region Feats
+        public static readonly Trait BodyguardCharge = ModManager.RegisterTrait("Bodyguard's Charge",
+            new TraitProperties("Bodyguard's Charge", false));
+        #endregion
     }
 
-    public static class CommonRequirements
+    public static class Tooltips
     {
-        public static CreatureTargetingRequirement WearingMediumOrHeavyArmor()
+        public static readonly Func<string, string> CommonWeaponSpec = RegisterTooltipInserter(
+            "Guardian.Common.WeaponSpecialization",
+            "{b}Weapon Specialization{/b}\n{i}Common class feature{/i}\nYou deal 2 additional damage with weapons and unarmed attacks in which you are an expert; this damage increases to 3 if you're a master, and to 4 if you're legendary.");
+        public static readonly Func<string, string> CommonGreaterWeaponSpec = RegisterTooltipInserter(
+            "Guardian.Common.GreaterWeaponSpecialization",
+            "{b}Greater Weapon Specialization{/b}\n{i}Common class feature{/i}\nYour damage from weapon specialization increases to 4 with weapons and unarmed attacks in which you're expert, 6 if you're a master, and 8 if you're legendary.");
+        public static readonly Func<string, string> ActionTaunt = RegisterTooltipInserter(
+            "Guardian.Actions.Taunt",
+            "{b}Taunt{/b} {icon:Action}\n{i}Concentrate, (Visual or Auditory){/i}\nChoose an enemy within 30 feet to be your taunted enemy. If your taunted enemy takes a hostile action that includes at least one of your allies but doesn't include you, they take a –1 circumstance penalty to their attack rolls and DCs for that action, and they also become off-guard until the start of their next turn.\n\nYour enemy remains taunted until the start of your next turn, and you can have only one Taunt in effect at a time. Taunting a new enemy ends this effect on any current target.\n\nTaunt gains the auditory trait, visual trait, or both, depending on how you draw the target's attention.");
+        public static readonly Func<string, string> ActionInterceptAttack = RegisterTooltipInserter(
+            "Guardian.Actions.InterceptAttack",
+            "{b}Intercept Attack{/b} {icon:Reaction}\n{b}Trigger{/b} An ally within 10 feet of you takes physical damage.\n\nYou can Step, but you must end your movement adjacent to the triggering ally. You take the damage instead of the triggering ally. Apply your own immunities, weaknesses, and resistances to the damage, not the ally's.\n\n{b}Special{/b} You can extend this ability to an ally within 15 feet of you if the damage comes from your taunted enemy. If this ally is farther than you can Step to reach, you can Stride instead of Stepping; you still must end the movement adjacent to your ally.");
+        public static readonly Func<string, string> ArmorResting = RegisterTooltipInserter(
+            "Guardian.Feature.GuardiansArmorResting",
+            "{b}Resting in Armor{/b}\n{i}Common rule{/i}\nSleeping in armor is uncomfortable, and would lead to poor-quality sleep. Some encounters occur while the party is sleeping. If you aren't able to sleep in armor, you won't have your armor donned at the start of combat.");
+        public static readonly Func<string, string> FeatureToughToKill = RegisterTooltipInserter(
+            "Guardian.Feature.ToughToKill",
+            "{b}Tough to Kill{/b}\n{i}Level 3 Guardian feature{/i}\nYou gain the Diehard general feat {i}(you should retrain it if you already have it){/i}. Additionally, the first time each day you'd be reduced to dying 3 or higher, you stay at dying 2 instead.");
+        public static readonly Func<string, string> FeatureReactionTime = RegisterTooltipInserter(
+            "Guardian.Feature.ReactionTime",
+            "{b}Reaction Time{/b}\n{i}Level 7 Guardian feature{/i}\nAt the start of combat and for each of your turns, you gain an additional reaction that you can use only for reactions from guardian feats or class features (including Shield Block).");
+        public static readonly Func<string, string> FeatureBattleHardened = RegisterTooltipInserter(
+            "Guardian.Feature.BattleHardened",
+            "{b}Battle Hardened{/b}\n{i}Level 9 Guardian feature{/i}\nYour proficiency rank for Fortitude saves increases to master; when you roll a success on a Fortitude save, you get a critical success instead.");
+        public static readonly Func<string, string> FeatureUnyieldingResolve = RegisterTooltipInserter(
+            "Guardian.Feature.UnyieldingResolve",
+            "{b}Unyielding Resolve{/b}\n{i}Level 17 Guardian feature{/i}\nYour proficiency rank for Will saves increases to master; when you roll a success on a Will save, you get a critical success instead.");
+        public static readonly Func<string, string> FeatureGuardianMastery = RegisterTooltipInserter(
+            "Guardian.Feature.GuardianMastery",
+            "{b}Guardian Mastery{/b}\n{i}Level 19 Guardian feature{/i}\nWhile wearing armor, when you attempt a Reflex save, you can add your armor's item bonus to AC instead of your Dexterity modifier if it's higher; if your armor has the bulwark trait, increase this bonus by 1. If you get a success when you do this, you get a critical success instead.");
+        
+        public static Func<string, string> RegisterTooltipInserter(string tooltipName, string tooltipDescription)
         {
-            return new LegacyCreatureTargetingRequirement((a,_) =>
-                (a.BaseArmor?.HasTrait(Trait.MediumArmor) ?? false) || (a.BaseArmor?.HasTrait(Trait.HeavyArmor) ?? false)
-                    ? Usability.Usable
-                    : Usability.NotUsable("must be wearing medium or heavy armor"));
+            ModManager.RegisterInlineTooltip(tooltipName, tooltipDescription);
+            return input => "{tooltip:" + tooltipName + "}" + input + "{/}";
         }
     }
 }
