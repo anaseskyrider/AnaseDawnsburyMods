f83997bd17307a53966a08cbc73b3f9e18c2853a
Anase Skyrider <anaseskyrider@gmail.com>
2025-11-21 16:29:33 -0500

    Fix initializer
    Enums and mod options will be loaded before entering any encounters.

--- More Basic Actions/ModData.cs
+++ More Basic Actions/ModData.cs
@@ -5,6 +5,7 @@ using Dawnsbury.Core.Mechanics;
 using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Core.Possibilities;
 using Dawnsbury.Display.Illustrations;
+using Dawnsbury.IO;
 using Dawnsbury.Modding;
 
 namespace Dawnsbury.Mods.MoreBasicActions;
@@ -15,32 +16,43 @@ public static class ModData
     
     public static void LoadData()
     {
-        ////////////////
-        // Action IDs //
-        ////////////////
-        // Ensures compatibility with other mods registering the same ID, regardless of load order.
-        ActionIds.PrepareToAid = ModManager.TryParse("PrepareToAid", out ActionId prepareAid)
-            ? prepareAid
-            : ModManager.RegisterEnumMember<ActionId>("PrepareToAid");
-        
-        ActionIds.AidReaction = ModManager.TryParse("AidReaction", out ActionId aidReaction)
-            ? aidReaction
-            : ModManager.RegisterEnumMember<ActionId>("AidReaction");
-        
-        ActionIds.Reposition = ModManager.TryParse("Reposition", out ActionId reposition)
-            ? reposition
-            : ModManager.RegisterEnumMember<ActionId>("Reposition");
+        // Force initializer to register enums
+        ActionIds.FixInit = true;
+        BooleanOptions.FixInit = true;
+        FeatNames.FixInit = true;
+        PossibilitySectionIds.FixInit = true;
+        QEffectIds.FixInit = true;
+        SubmenuIds.FixInit = true;
+        Traits.FixInit = true;
+    }
+
+    /// <summary>
+    /// Registers the source enum to the game, or returns the original if it's already registered.
+    /// </summary>
+    /// <param name="technicalName">The technicalName string of the enum being registered.</param>
+    /// <typeparam name="T">The enum being registered to.</typeparam>
+    /// <returns>The newly registered enum.</returns>
+    public static T SafelyRegister<T>(string technicalName) where T : struct, Enum
+    {
+        return ModManager.TryParse<T>(technicalName, out T alreadyRegistered)
+            ? alreadyRegistered
+            : ModManager.RegisterEnumMember<T>(technicalName);
     }
 
     public static class ActionIds
     {
-        public static ActionId PrepareToAid;
-        public static ActionId AidReaction;
-        public static readonly ActionId Ready = ModManager.RegisterEnumMember<ActionId>("Ready");
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
+        public static readonly ActionId PrepareToAid = SafelyRegister<ActionId>("PrepareToAid");
+        public static readonly ActionId AidReaction = SafelyRegister<ActionId>("AidReaction");
+        public static readonly ActionId Ready = SafelyRegister<ActionId>("Ready");
         public static readonly ActionId HelpUp = ModManager.RegisterEnumMember<ActionId>("HelpUp");
         public static readonly ActionId QuickRepair = ModManager.RegisterEnumMember<ActionId>("QuickRepair");
         public static readonly ActionId LongJump = ModManager.RegisterEnumMember<ActionId>("LongJump");
-        public static ActionId Reposition;
+        public static readonly ActionId Reposition = SafelyRegister<ActionId>("Reposition");
     }
     
     /// <summary>
@@ -48,6 +60,11 @@ public static class ModData
     /// </summary>
     public static class BooleanOptions
     {
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
         /// <summary>Allow untrained Prepare to Aid actions.</summary>
         public static readonly string UntrainedAid = RegisterBooleanOption(
             IdPrepend+"UntrainedAid",
@@ -55,7 +72,7 @@ public static class ModData
             "Enable untrained Prepare to Aid options when choosing what skills to prepare to aid.",
             false);
         /// <summary>Lower the DC for the Aid reaction.</summary>
-        public static readonly string AidDCIs15 = RegisterBooleanOption(
+        public static string AidDCIs15 = RegisterBooleanOption(
             IdPrepend+"AidDCIs15",
             "More Basic Actions: Reduce Aid DC",
             "The DC to Aid is normally 20. If enabled, the DC is reduced to 15 instead.",
@@ -102,6 +119,11 @@ public static class ModData
     
     public static class FeatNames
     {
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
         public static readonly FeatName CooperativeNature = ModManager.RegisterFeatName(IdPrepend+"Human.CooperativeNature", "Cooperative Nature");
         public static readonly FeatName QuickRepair = ModManager.RegisterFeatName(IdPrepend+"QuickRepair", "Quick Repair");
         public static readonly FeatName QuickJump = ModManager.RegisterFeatName(IdPrepend+"QuickJump", "Quick Jump");
@@ -122,6 +144,11 @@ public static class ModData
     
     public static class PossibilitySectionIds
     {
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
         public static readonly PossibilitySectionId AidSkills = ModManager.RegisterEnumMember<PossibilitySectionId>("AidSkills");
         public static readonly PossibilitySectionId AidAttacks = ModManager.RegisterEnumMember<PossibilitySectionId>("AidAttacks");
         public static readonly PossibilitySectionId Ready = ModManager.RegisterEnumMember<PossibilitySectionId>("Ready");
@@ -129,18 +156,33 @@ public static class ModData
     
     public static class QEffectIds
     {
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
         public static readonly QEffectId PreparedToAid = ModManager.RegisterEnumMember<QEffectId>("Prepared to Aid");
         public static readonly QEffectId Readied = ModManager.RegisterEnumMember<QEffectId>("Readied");
     }
 
     public static class SubmenuIds
     {
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
         public static readonly SubmenuId PrepareToAid = ModManager.RegisterEnumMember<SubmenuId>("PrepareToAid");
         public static readonly SubmenuId Ready = ModManager.RegisterEnumMember<SubmenuId>("Ready");
     }
     
     public static class Traits
     {
+        /// <summary>
+        /// Does nothing except fixes my initializer-based assignments.
+        /// </summary>
+        public static bool FixInit = false;
+        
         public static readonly Trait MoreBasicActions = ModManager.RegisterTrait("MoreBasicActions", new TraitProperties("More Basic Actions", true));
         public static readonly Trait Brace = ModManager.RegisterTrait("Brace", new TraitProperties("Brace", true, "When you Ready to Strike an opponent that moves within your reach, until the start of your next turn Strikes made as part of a reaction with the brace weapon deal an additional 2 precision damage for each weapon damage die it has."));
         /// This attack is a reactive attack, but it has and contributes to MAP. (Used to differentiate regular Strikes from a Brace weapon with reaction Strikes). 
