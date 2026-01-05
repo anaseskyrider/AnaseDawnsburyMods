f837fc19dc0912692643bdee04158aae9ac7dccc
Anase Skyrider <anaseskyrider@gmail.com>
2026-01-05 04:40:39 -0500

    Update LibraryOfAnase to v1.3

--- More Shields/LibraryOfAnase.cs
+++ More Shields/LibraryOfAnase.cs
@@ -4,8 +4,11 @@ using Dawnsbury.Campaign.Path;
 using Dawnsbury.Core;
 using Dawnsbury.Core.Animations;
 using Dawnsbury.Core.CharacterBuilder;
+using Dawnsbury.Core.CharacterBuilder.FeatsDb.Spellbook;
 using Dawnsbury.Core.CharacterBuilder.Library;
+using Dawnsbury.Core.CharacterBuilder.Spellcasting;
 using Dawnsbury.Core.CombatActions;
+using Dawnsbury.Core.Coroutines.Options;
 using Dawnsbury.Core.Coroutines.Requests;
 using Dawnsbury.Core.Creatures;
 using Dawnsbury.Core.Mechanics;
@@ -13,6 +16,7 @@ using Dawnsbury.Core.Mechanics.Core;
 using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Core.Mechanics.Treasure;
 using Dawnsbury.Core.Possibilities;
+using Dawnsbury.Modding;
 
 namespace Dawnsbury.Mods.MoreShields;
 
@@ -20,6 +24,8 @@ namespace Dawnsbury.Mods.MoreShields;
 /// Anase's library of helpful code functions. Contains a wide array of broadly useful functions rather than specialized logic.
 /// </summary>
 /// <list type="bullet">
+/// <item>v1.3: Added CombatAction.HasAllTraits, CombatAction.HasAnyTraits, OfferOptions2 with variants for ActionPossibility and Possibility.</item>
+/// <item>v1.2: Added CreateSpellLink(SpellId, Trait, int). Refactored into Extension blocks.</item>
 /// <item>v1.1: Added int.WithColor(), QEffect.With(), CombatAction.With(), Item.HasAllTraits, Item.HasAnyTraits.</item>
 /// <item>v1.0: Initial.</item>
 /// </list>
@@ -28,116 +34,140 @@ public static class LibraryOfAnase
 {
     #region Extensions
 
-    /// <summary>
-    /// Runs any modifications to the QEffect in one code block, similar to Zone.With().
-    /// </summary>
-    public static QEffect With(this QEffect qfThis, Action<QEffect> changes)
+    extension(CombatAction caThis)
     {
-        changes.Invoke(qfThis);
-        return qfThis;
-    }
-
-    /// <summary>
-    /// Runs any modifications to the CombatAction in one code block, similar to Zone.With().
-    /// </summary>
-    public static CombatAction With(this CombatAction caThis, Action<CombatAction> changes)
-    {
-        changes.Invoke(caThis);
-        return caThis;
-    }
-
-    /// <summary>
-    /// Returns whether the item has all the passed traits.
-    /// </summary>
-    public static bool HasAllTraits(this Item item, params Trait[] traits) =>
-        item.Traits.All(traits.Contains);
-    
-    /// <summary>
-    /// Returns whether the item has any of the passed traits.
-    /// </summary>
-    public static bool HasAnyTraits(this Item item, params Trait[] traits) =>
-        item.Traits.Any(traits.Contains);
-
-    /// <summary>
-    /// Causes a QEffect to put an action in the Offense section of the creature stat block using the given action name, short description, and cost; but without listing any attack statistics. Useful for "metastrike" actions such as Power Attack, displaying them only once.
-    /// </summary>
-    public static void WithDisplayActionInOffenseSection(this QEffect qfFeat, string actionName, string shortDescription, int cost = 1)
-    {
-        qfFeat.ProvideActionIntoPossibilitySection += (qfThis, section) =>
+        /// <summary>
+        /// Runs any modifications to the CombatAction in one code block, similar to Zone.With().
+        /// </summary>
+        public CombatAction With(Action<CombatAction> changes)
         {
-            // Inserts into invisible section
-            if (section.PossibilitySectionId != PossibilitySectionId.InvisibleActions)
-                return null;
-            CombatAction statBlockOnly = CombatAction.CreateSimple(
-                    qfThis.Owner,
-                    actionName,
-                    [])
-                .WithShortDescription(shortDescription)
-                .WithActionCost(cost);
-            statBlockOnly.Illustration = IllustrationName.None;
-            return new ActionPossibility(statBlockOnly);
-        };
-    }
+            changes.Invoke(caThis);
+            return caThis;
+        }
+        
+        /// <summary>
+        /// Returns whether the CombatAction has all the passed traits.
+        /// </summary>
+        public bool HasAllTraits(params Trait[] traits) =>
+            caThis.Traits.All(traits.Contains);
 
-    /// <summary>
-    /// Adds an extra effect to an action that occurs when you both hit and deal at least 1 point of damage to a creature.
-    /// </summary>
-    /// <para>
-    /// Only meaningfully works for actions which have an attack roll. This utilizes <see cref="CombatAction.WithPrologueEffectOnChosenTargetsBeforeRolls"/>, which has smart delegate combination (this code will execute after the previous behavior). If you need to overwrite this function before adding this functionality, first call
-    /// <code>CombatAction.EffectOnChosenTargetsBeforeRolls = null;</code>
-    /// before doing so.
-    /// </para>
-    /// <param name="action">The CombatAction this function extends off of.</param>
-    /// <param name="doWhat">The code to execute once the action has hit and dealt damage. Uses the same parameters for this lambda as <see cref="QEffect.AfterYouDealDamage"/>.</param>
-    /// <returns></returns>
-    public static CombatAction WithHitAndDealDamage(this CombatAction action, Func<Creature, CombatAction, Creature, Task> doWhat)
-    {
-        return action.WithPrologueEffectOnChosenTargetsBeforeRolls(async (innerAction, self, targets) =>
+        /// <summary>
+        /// Returns whether the CombatAction has any of the passed traits.
+        /// </summary>
+        public bool HasAnyTraits(params Trait[] traits) =>
+            caThis.Traits.Any(traits.Contains);
+
+        /// <summary>
+        /// Adds an extra effect to an action that occurs when you both hit and deal at least 1 point of damage to a creature.
+        /// </summary>
+        /// <para>
+        /// Only meaningfully works for actions which have an attack roll. This utilizes <see cref="CombatAction.WithPrologueEffectOnChosenTargetsBeforeRolls"/>, which has smart delegate combination (this code will execute after the previous behavior). If you need to overwrite this function before adding this functionality, first call
+        /// <code>CombatAction.EffectOnChosenTargetsBeforeRolls = null;</code>
+        /// before doing so.
+        /// </para>
+        /// <param name="doWhat">The code to execute once the action has hit and dealt damage. Uses the same parameters for this lambda as <see cref="QEffect.AfterYouDealDamage"/>.</param>
+        /// <returns></returns>
+        public CombatAction WithHitAndDealDamage(Func<Creature, CombatAction, Creature, Task> doWhat)
         {
-            // Initialize to capture reference in scope
-            QEffect doAfter = new QEffect()
+            return caThis.WithPrologueEffectOnChosenTargetsBeforeRolls(async (innerAction, self, targets) =>
             {
-                Name = "[AFTER YOU DEAL DAMAGE WITH: " + innerAction.Name + "]",
-                ExpiresAt = ExpirationCondition.ExpiresAtEndOfYourTurn, // Fallback
-            };
-            doAfter.AfterYouDealDamage = async (self2, innerAction2, target) =>
-            {
-                if (innerAction2 != innerAction
-                    || target != targets.ChosenCreature
-                    || innerAction2.CheckResult < CheckResult.Success
-                    || innerAction2.Item != action.Item)
-                    return;
+                // Initialize to capture reference in scope
+                QEffect doAfter = new QEffect()
+                {
+                    Name = "[AFTER YOU DEAL DAMAGE WITH: " + innerAction.Name + "]",
+                    ExpiresAt = ExpirationCondition.ExpiresAtEndOfYourTurn, // Fallback
+                };
+                doAfter.AfterYouDealDamage = async (self2, innerAction2, target) =>
+                {
+                    if (innerAction2 != innerAction
+                        || target != targets.ChosenCreature
+                        || innerAction2.CheckResult < CheckResult.Success
+                        || innerAction2.Item != caThis.Item)
+                        return;
 
-                await doWhat.Invoke(self2, innerAction2, target);
+                    await doWhat.Invoke(self2, innerAction2, target);
 
-                doAfter.ExpiresAt = ExpirationCondition.Immediately;
-            };
-            self.AddQEffect(doAfter);
-        });
+                    doAfter.ExpiresAt = ExpirationCondition.Immediately;
+                };
+                self.AddQEffect(doAfter);
+            });
+        }
     }
     
-    /// <summary>
-    /// Adds color tags to the given string.
-    /// </summary>
-    /// <param name="text">The original string that this function extends off of.</param>
-    /// <param name="color">The color, formatted as "Green", to be added to the string.</param>
-    /// <returns></returns>
-    public static string WithColor(this string text, string color)
+    extension(QEffect qfThis)
     {
-        color = color.Capitalize();
-        return "{"+color+"}" + text + "{/"+color+"}";
+        /// <summary>
+        /// Runs any modifications to the QEffect in one code block, similar to Zone.With().
+        /// </summary>
+        public QEffect With(Action<QEffect> changes)
+        {
+            changes.Invoke(qfThis);
+            return qfThis;
+        }
+
+        /// <summary>
+        /// Causes a QEffect to put an action in the Offense section of the creature stat block using the given action name, short description, and cost; but without listing any attack statistics. Useful for "metastrike" actions such as Power Attack, displaying them only once.
+        /// </summary>
+        public void WithDisplayActionInOffenseSection(string actionName, string shortDescription, int cost = 1)
+        {
+            qfThis.ProvideActionIntoPossibilitySection += (qfThis1, section) =>
+            {
+                // Inserts into invisible section
+                if (section.PossibilitySectionId != PossibilitySectionId.InvisibleActions)
+                    return null;
+                CombatAction statBlockOnly = CombatAction.CreateSimple(
+                        qfThis1.Owner,
+                        actionName,
+                        [])
+                    .WithShortDescription(shortDescription)
+                    .WithActionCost(cost);
+                statBlockOnly.Illustration = IllustrationName.None;
+                return new ActionPossibility(statBlockOnly);
+            };
+        }
     }
-    
-    /// <summary>
-    /// Adds color tags to the given integer.
-    /// </summary>
-    /// <param name="number">The integer that this function extends off of.</param>
-    /// <param name="color">The color, formatted as "Green", to be added to the string.</param>
-    /// <returns></returns>
-    public static string WithColor(this int number, string color)
+
+    extension(Item item)
     {
-        color = color.Capitalize();
-        return "{"+color+"}" + number + "{/"+color+"}";
+        /// <summary>
+        /// Returns whether the item has all the passed traits.
+        /// </summary>
+        public bool HasAllTraits(params Trait[] traits) =>
+            item.Traits.All(traits.Contains);
+
+        /// <summary>
+        /// Returns whether the item has any of the passed traits.
+        /// </summary>
+        public bool HasAnyTraits(params Trait[] traits) =>
+            item.Traits.Any(traits.Contains);
+    }
+
+    extension(string text)
+    {
+        /// <summary>
+        /// Adds color tags to the given string.
+        /// </summary>
+        /// <param name="color">The color, formatted as "Green", to be added to the string.</param>
+        /// <returns></returns>
+        public string WithColor(string color)
+        {
+            color = color.Capitalize();
+            return "{"+color+"}" + text + "{/"+color+"}";
+        }
+    }
+
+    extension(int number)
+    {
+        /// <summary>
+        /// Adds color tags to the given integer.
+        /// </summary>
+        /// <param name="color">The color, formatted as "Green", to be added to the string.</param>
+        /// <returns></returns>
+        public string WithColor(string color)
+        {
+            color = color.Capitalize();
+            return "{"+color+"}" + number + "{/"+color+"}";
+        }
     }
     
     /// <summary>
@@ -156,10 +186,64 @@ public static class LibraryOfAnase
         });
         cinema.TutorialBubble = null;
     }
+    
+    
+    public static int DetermineCircumstanceBonusThresholdNeededToUpgrade(this CheckBreakdownResult resultBreakdown)
+    {
+        CheckBreakdown breakdown = resultBreakdown.CheckBreakdown;
+        int rollTotal = breakdown.TotalCheckBonus + resultBreakdown.D20Roll;
+        CheckResult result = CheckResult.CriticalSuccess;
+        int thresholdToUpgrade = 1000;
+        // Is not crit
+        if (rollTotal < breakdown.TotalDC + 10)
+        {
+            result = CheckResult.Success;
+            thresholdToUpgrade = (breakdown.TotalDC + 10) - rollTotal;
+        }
+        // Is failure
+        if (rollTotal < breakdown.TotalDC)
+        {
+            result = CheckResult.Failure;
+            thresholdToUpgrade = (breakdown.TotalDC) - rollTotal;
+        }
+        // Is fumble
+        if (rollTotal <= breakdown.TotalDC - 10)
+        {
+            result = CheckResult.CriticalFailure;
+            thresholdToUpgrade = (breakdown.TotalDC - 9) - rollTotal;
+        }
+        // Is nat-1
+        if (resultBreakdown.D20Roll == 1)
+        {
+            if (result == CheckResult.CriticalFailure)
+                thresholdToUpgrade += 10;
+        }
+        if (resultBreakdown.CheckBreakdown.DefenseBonuses == null
+            || resultBreakdown.CheckBreakdown.DefenseBonuses.Count == 0)
+            return thresholdToUpgrade;
+        int num = resultBreakdown.CheckBreakdown.DefenseBonuses.Max(sb =>
+            sb is not { BonusType: BonusType.Circumstance }
+            || sb.Amount <= 0
+                ? 0
+                : sb.Amount);
+        return thresholdToUpgrade + num;
+    }
 
     #endregion
 
     #region Statics
+
+    /// <summary>
+    /// Alternative overload for <see cref="AllSpells.CreateSpellLink"/> which includes the spell's level.
+    /// </summary>
+    public static string CreateSpellLink(SpellId spell, Trait classOfOrigin, int spellLevel)
+    {
+        Spell template = AllSpells.CreateModernSpellTemplate(spell, classOfOrigin, spellLevel);
+        string str = template.CombatActionSpell.SpellInformation != null
+            ? ":" + template.CombatActionSpell.SpellInformation.ClassOfOrigin.ToStringOrTechnical() + ":" + spellLevel
+            : "";
+        return $"{{i}}{{link:{template.SpellId.ToStringOrTechnical()}{str}}}{template.Name.ToLower()}{{/link}}{{/i}}";
+    }
 
     /// <summary>
     /// If a character sheet is available at the execution time of this function, it will return a character sheet of a party member either during campaign play or in free encounter play.
@@ -173,6 +257,65 @@ public static class LibraryOfAnase
         else if (CharacterLibrary.Instance is { } library)
             hero = library.SelectedRandomEncounterParty[index];
         return hero;
+    }
+
+    /// <summary>
+    /// Consolidates code commonly seen when using <see cref="GameLoop.OfferOptions"/>, with extra handling for when OfferOptions is used off-turn.
+    /// </summary>
+    public static async Task OfferOptions2(Creature self, Func<ActionPossibility, bool> filter)
+    {
+        Possibilities poss =  Possibilities
+            .Create(self)
+            .Filter(ap =>
+            {
+                
+                ap.CombatAction.ActionCost = 0;
+                if (!filter.Invoke(ap.CombatAction))
+                    return false;
+                ap.RecalculateUsability();
+                return true;
+            });
+        
+        Creature? active = self.Battle.ActiveCreature;
+        self.Battle.ActiveCreature = self;
+        self.Possibilities = poss;
+        
+        List<Option> actions = await self.Battle.GameLoop.CreateActions(
+            self,
+            poss,
+            null);
+        self.Battle.GameLoopCallback.AfterActiveCreaturePossibilitiesRegenerated();
+        await self.Battle.GameLoop.OfferOptions(self, actions, true);
+        
+        self.Battle.ActiveCreature = active;
+    }
+
+    /// <summary>
+    /// Consolidates code commonly seen when using <see cref="GameLoop.OfferOptions"/>, with extra handling for when OfferOptions is used off-turn.
+    /// </summary>
+    public static async Task OfferOptions2(Creature self, Func<Possibility, bool> filter)
+    {
+        Possibilities poss =  Possibilities
+            .Create(self)
+            .FilterAnyPossibility(poss =>
+            {
+                if (!filter.Invoke(poss))
+                    return false;
+                return true;
+            });
+        
+        Creature? active = self.Battle.ActiveCreature;
+        self.Battle.ActiveCreature = self;
+        self.Possibilities = poss;
+        
+        List<Option> actions = await self.Battle.GameLoop.CreateActions(
+            self,
+            poss,
+            null);
+        self.Battle.GameLoopCallback.AfterActiveCreaturePossibilitiesRegenerated();
+        await self.Battle.GameLoop.OfferOptions(self, actions, true);
+        
+        self.Battle.ActiveCreature = active;
     }
 
     #endregion
