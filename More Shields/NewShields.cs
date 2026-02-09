597b420d8a7766dfb2030d288b127bef61c90413
Anase Skyrider <anaseskyrider@gmail.com>
2026-02-09 04:39:08 -0500

    Reformat new shield item descriptions
    Uses new extension function to add flavor-text formatting.

--- More Shields/NewShields.cs
+++ More Shields/NewShields.cs
@@ -37,7 +37,7 @@ public static class NewShields
                     0, 1,
                     ModData.Traits.MoreShields, Trait.Shield, Trait.Martial, ModData.Traits.LightShield, Trait.Worn, ModData.Traits.WornShield)
                 .WithMainTrait(ModData.Traits.Buckler)
-                .WithDescription("This very small shield is a favorite of duelists and quick, lightly armored warriors. It's typically made of steel and strapped to your forearm.")
+                .WithDescription("This very small shield is a favorite of duelists and quick, lightly armored warriors. It's typically made of steel and strapped to your forearm.", "")
                 .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Bludgeoning))
                 .WithShieldProperties(3));
         ModData.ItemNames.FortressShield = ModManager.RegisterNewItemIntoTheShop(
@@ -49,7 +49,7 @@ public static class NewShields
                     1, 20,
                     ModData.Traits.MoreShields, Trait.Shield, Trait.Martial, ModData.Traits.HeavyShield, ModData.Traits.CoverShield, ModData.Traits.Hefty14)
                 .WithMainTrait(ModData.Traits.FortressShield)
-                .WithDescription("Also known as portable walls, these thick and heavy shields are slightly larger than tower shields. Like tower shields, they're typically made from wood reinforced with metal, but many are made from larger amounts of metal or even stone.")
+                .WithDescription("Also known as portable walls, these thick and heavy shields are slightly larger than tower shields. Like tower shields, they're typically made from wood reinforced with metal, but many are made from larger amounts of metal or even stone.", "")
                 .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Bludgeoning))
                 .WithShieldProperties(6));
         ModData.ItemNames.MeteorShield = ModManager.RegisterNewItemIntoTheShop(
@@ -61,7 +61,7 @@ public static class NewShields
                     0, 4,
                     ModData.Traits.MoreShields, Trait.Shield, Trait.Martial, Trait.Thrown30Feet, ModData.Traits.MediumShield)
                 .WithMainTrait(ModData.Traits.MeteorShield)
-                .WithDescription("Meteor shields are specifically designed with throwing in mind. A meteor shield is made from thin steel and has quick-release straps, allowing for easy, long-distance throws.")
+                .WithDescription("Meteor shields are specifically designed with throwing in mind. A meteor shield is made from thin steel and has quick-release straps, allowing for easy, long-distance throws.", "")
                 .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Bludgeoning)
                 {
                     VfxStyle = new VfxStyle(1, ProjectileKind.Arrow, IllustrationName.WoodenShieldBoss)
@@ -76,7 +76,7 @@ public static class NewShields
                     1, 5,
                     ModData.Traits.MoreShields, Trait.Shield, Trait.Martial, ModData.Traits.LightShield, Trait.Worn, ModData.Traits.WornShield)
                 .WithMainTrait(ModData.Traits.HeavyRondache)
-                .WithDescription("Similar in size to a buckler, this shield is intended to absorb blows instead of deflecting attacks. It features multiple layers of metal and is reinforced with additional wood.")
+                .WithDescription("Similar in size to a buckler, this shield is intended to absorb blows instead of deflecting attacks. It features multiple layers of metal and is reinforced with additional wood.", "")
                 .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Bludgeoning))
                 .WithShieldProperties(5));
         ModData.ItemNames.CastersTarge = ModManager.RegisterNewItemIntoTheShop(
@@ -102,7 +102,8 @@ public static class NewShields
                         ac)
                     .WithMainTrait(ModData.Traits.CastersTarge)
                     .WithDescription(
-                        $"This small shield is made from wood. It features a special panel of parchment along the inside surface that allows it to store {scrollLimit} spell {S.PluralizeIf("scroll", scrollLimit)}.\n\nYou must have a free hand to activate a scroll, and can only do so once per encounter.")
+                        "This small shield is made from wood. It features a special panel along the inside surface that holds scrolls.",
+                        $"You can store {scrollLimit} spell {S.PluralizeIf("scroll", scrollLimit)} in this shield {{i}}(out-of-combat, drag a scroll onto this shield to store it){{/i}}. You must have a free hand to activate a scroll, and can only do so once per encounter {{i}}(this makes the other scrolls inaccessible){{i}}.")
                     .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Bludgeoning))
                     .WithShieldProperties(hrd)
                     .WithStoresItem((targe, scroll) =>
