d631262c24b66e5b5ad3875bab1d33aaa1383e73
Anase Skyrider <anaseskyrider@gmail.com>
2026-07-15 08:35:51 -0400

    Expand compatibility of modded Reflexive Shield feats

--- Guardian Class/ModLoader.cs
+++ Guardian Class/ModLoader.cs
@@ -7,6 +7,7 @@ using Dawnsbury.Core.CharacterBuilder.Library;
 using Dawnsbury.Core.CombatActions;
 using Dawnsbury.Core.Coroutines.Requests;
 using Dawnsbury.Core.Mechanics;
+using Dawnsbury.Core.Mechanics.Enumerations;
 using Dawnsbury.Core.Possibilities;
 using Dawnsbury.Display.Illustrations;
 using Dawnsbury.Modding;
@@ -32,8 +33,21 @@ public static class ModLoader
         // Update class language
         LoadOrder.AtEndOfLoadingSequence += () =>
         {
+            // Adjust class text
             Feat? guardianClass = AllFeats.All.FirstOrDefault(ft => ft.FeatName == ModData.FeatNames.GuardianClass);
             guardianClass!.RulesText = guardianClass.RulesText.Replace("Ability boosts", "Attribute boosts");
+            
+            // Add modded feats
+            foreach (TrueFeat feat in AllFeats.All
+                         .Where(ft => ft is TrueFeat)
+                         .Cast<TrueFeat>()
+                         .Where(tf =>
+                             (tf.Name.Contains("Reflexive Shield")) // Will include Improved Reflexive Shield
+                             && !tf.HasTrait(Trait.Archetype)))
+                feat.WithAllowsForAdditionalClassTrait(ModData.Traits.Guardian);
+            /*if (ModManager.TryParse("MoreDedications.Class.Fighter.ReflexiveShield", out FeatName refShield))
+                (AllFeats.GetFeatByFeatName(refShield) as TrueFeat)!
+                    .WithAllowsForAdditionalClassTrait(ModData.Traits.Guardian);*/
         };
     }
 }
