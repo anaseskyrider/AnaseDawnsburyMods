0b1da4027970403fd3f4c6c3cbb8e523ff1a845e
Anase Skyrider <anaseskyrider@gmail.com>
2026-08-17 07:10:03 -0400

    Rewrite Long Jump
    - Includes tile tooltips
    - More detailed, better-formatted distance breakdowns of the rolled jump distance
    - Slight improvements for large creatures.
    - Acceptable deviation from Stride line increased from 30 to 45 degrees.

--- More Basic Actions/LongJump.cs
+++ More Basic Actions/LongJump.cs
@@ -1,6 +1,5 @@
-using Dawnsbury.Core;
+using Dawnsbury.Auxiliary;
 using Dawnsbury.Core.Animations.Movement;
-using Dawnsbury.Core.CharacterBuilder;
 using Dawnsbury.Core.CharacterBuilder.Feats;
 using Dawnsbury.Core.CharacterBuilder.FeatsDb.Common;
 using Dawnsbury.Core.CharacterBuilder.FeatsDb.TrueFeatDb.Archetypes;
@@ -15,7 +14,6 @@ using Dawnsbury.Core.Mechanics.Targeting.Targets;
 using Dawnsbury.Core.Possibilities;
 using Dawnsbury.Core.Tiles;
 using Dawnsbury.Display;
-using Dawnsbury.Display.Illustrations;
 using Dawnsbury.Display.Text;
 using Dawnsbury.Modding;
 using Microsoft.Xna.Framework;
@@ -25,7 +23,7 @@ namespace Dawnsbury.Mods.MoreBasicActions;
 public static class LongJump
 {
     /// The acceptable range of deviation from the initial Stride for the follow-up Leap.
-    public const int LeapRange = 30;
+    public const int LEAP_RANGE = 45;
 
     public static void LoadLongJump()
     {
@@ -38,8 +36,7 @@ public static class LongJump
                 Key = "LongJumpGranter",
                 ProvideActionIntoPossibilitySection = (qfThis, section) =>
                 {
-                    PossibilitySectionId sectionId = PossibilitySectionId.Movement;
-                    if (section.PossibilitySectionId != sectionId)
+                    if (section.PossibilitySectionId != PossibilitySectionId.Movement)
                         return null;
 
                     return (ActionPossibility) CreateLongJump(
@@ -59,8 +56,7 @@ public static class LongJump
     {
         // Quick Jump
         yield return new TrueFeat(
-                ModData.FeatNames.QuickJump,
-                1,
+                ModData.FeatNames.QuickJump, 1,
                 null,
                 "High Jump costs a single action instead of 2, and no longer performs an initial Stride.",
                 [ModData.ModTrait, Trait.General, Trait.Skill])
@@ -70,8 +66,7 @@ public static class LongJump
         
         // Acrobat - Graceful Leaper
         yield return new TrueFeat(
-                ModData.FeatNames.GracefulLeaper,
-                7,
+                ModData.FeatNames.GracefulLeaper, 7,
                 "Mass and muscle are meaningless when you leap; only grace and balance matter.",
                 "You can use Acrobatics instead of Athletics to Long Jump if it's better.",
                 [ModData.ModTrait, Trait.Archetype, Trait.Skill])
@@ -85,21 +80,21 @@ public static class LongJump
     public static CombatAction CreateLongJump(Creature owner, bool hasQuickJump = false)
     {
         Skill bestSkill = owner.HasFeat(ModData.FeatNames.GracefulLeaper)
-            ? new Skill[] {Skill.Acrobatics, Skill.Athletics}.MaxBy(skill => owner.Skills.Get(skill))
+            ? new[] {Skill.Acrobatics, Skill.Athletics}.MaxBy(skill => owner.Skills.Get(skill))
             : Skill.Athletics;
+        
         return new CombatAction(
                 owner,
                 ModData.Illustrations.LongJump,
                 "Long Jump",
-                [ModData.ModTrait, Trait.Basic, Trait.Move, Trait.DoesNotProvoke],
+                [ModData.ModTrait, Trait.Move, Trait.DoesNotProvoke],
                 null!,
                 hasQuickJump
                     ? Target.Self() // Skips the Stride part
-                    : Target.Tile(
-                            (cr, t) =>
+                    : Target.Tile((cr, t) =>
                                 t.LooksFreeTo(cr)
                                 && t.DistanceTo(cr) > 1
-                                && cr.Occupies.HasLineOfEffectToIgnoreLesser(t) == CoverKind.None,
+                                && cr.Space.Tiles.All(t2 => t2.HasLineOfEffectToIgnoreLesser(t) == CoverKind.None),
                             (_, _) => int.MinValue)
                         .WithPathfindingGuidelines(cr =>
                             new PathfindingDescription { Squares = cr.Speed }))
@@ -117,19 +112,35 @@ public static class LongJump
             .WithActionId(ModData.ActionIds.LongJump) // Bonuses to checks to leap should use ActionId.Leap.
             .WithEffectOnChosenTargets(async (action, caster, targets) =>
             {
+                #region Minimum Stride
+
                 // Stride in a straight line
                 Tile? strideStart = null;
                 if (!hasQuickJump)
                 {
-                    strideStart = caster.Occupies;
-                    await caster.MoveTo(targets.ChosenTile!, action,
-                        new MovementStyle() { ShortestPath = true, MaximumSquares = 100 });
-                    if (caster.Occupies.DistanceTo(strideStart) < 2)
+                    strideStart = caster.Space.TopLeftTile;
+                    await caster.MoveTo(
+                        targets.ChosenTile!,
+                        action,
+                        new MovementStyle()
+                        {
+                            ShortestPath = true,
+                            MaximumSquares = 100,
+                            Shifting =
+                                (caster.HasEffect(QEffectId.Mobility)
+                                 || caster.HasEffect(QEffectId.IgnoreAoOWhenMoving))
+                                && !(targets.ChosenTile?.InIteration.RequiresProvokingAttackOfOpportunity ?? false)
+                        });
+                    if (caster.Space.TopLeftTile.DistanceTo(strideStart) < 2)
                     {
                         caster.Battle.Log("Long Jump disrupted. Failed to Stride at least 10 feet.");
                         return;
                     }
                 }
+
+                #endregion
+
+                #region Leap after Stride
 
                 // Leap after Stride
                 CombatAction leap = CommonCombatActions.Leap(
@@ -137,43 +148,93 @@ public static class LongJump
                         CalculateMaximumJumpDistance(caster))
                     .WithActionCost(0)
                     .WithExtraTrait(Trait.DoNotShowInCombatLog);
+                
                 // Must Leap in the same direction of your Stride
                 if (strideStart != null)
                 {
-                    Tile leapStart = caster.Occupies;
+                    Tile leapStart = caster.Space.CenterTile;
                     var angleFromStride = Math.Atan2(
-                        leapStart.Y - strideStart.Y,
-                        leapStart.X - strideStart.X)
-                        * (180.0 / Math.PI);
+                                              leapStart.Y - strideStart.Y,
+                                              leapStart.X - strideStart.X)
+                                          * (180.0 / Math.PI);
                     ((TileTarget)leap.Target).WithAdditionalTargetingRequirement((_, t) =>
                     {
                         var angleToLeap = Math.Atan2(
-                            t.Y - leapStart.Y,
-                            t.X - leapStart.X)
-                            * (180.0 / Math.PI);
-                        return angleToLeap <= angleFromStride+LeapRange
-                            && angleToLeap >= angleFromStride-LeapRange
+                                              t.Y - leapStart.Y,
+                                              t.X - leapStart.X)
+                                          * (180.0 / Math.PI);
+                        return angleToLeap <= angleFromStride+LEAP_RANGE
+                               && angleToLeap >= angleFromStride-LEAP_RANGE
                             ? Usability.Usable
                             : Usability.NotUsableOnThisCreature("Not in the same direction");
                     });
                 }
                 
-                leap.EffectOnChosenTargets = null;
+                // Prepare check data
+                TaggedCalculatedNumberProducer jumpSkill = owner.HasFeat(ModData.FeatNames.GracefulLeaper)
+                    ? TaggedChecks.SkillCheck(Skill.Athletics, Skill.Acrobatics)
+                    : TaggedChecks.SkillCheck(Skill.Athletics);
+                CheckBreakdown breakdownForTooltip = CombatActionExecution.BreakdownAttackForTooltip(
+                    leap.WithActiveRollSpecification(new ActiveRollSpecification(
+                        jumpSkill,
+                        new TaggedCalculatedNumberProducer((_, _, _) =>
+                            new CalculatedNumber(15, "Long Jump DC", [])))),
+                    caster);
+                
+                // Targeting tooltip showing chances of success
+                ((TileTarget)leap.Target).WithTooltip((target, tile) =>
+                {
+                    Creature self = target.OwnerAction.Owner;
+                    int distanceToTarget = self.DistanceTo(tile);
+
+                    List<int> rollDistances = [];
+                    for (int i = 1; i < 21; i++)
+                    {
+                        CheckResult result =
+                            i <= breakdownForTooltip.CritMisses ? CheckResult.CriticalFailure
+                            : i <= breakdownForTooltip.Misses ? CheckResult.Failure
+                            : i <= breakdownForTooltip.Hits ? CheckResult.Success
+                            : CheckResult.CriticalSuccess;
+                        int rollDistance = CalculateJumpDistanceResult(
+                            self, i + breakdownForTooltip.TotalCheckBonus, result);
+                        rollDistances.Add(rollDistance);
+                    }
+
+                    int atLeastThatDistance = rollDistances.Count(dist => dist >= distanceToTarget);
+
+                    return breakdownForTooltip.TooltipDescription
+                        .Replace(
+                            ProbabilityBar(
+                                breakdownForTooltip.CritMisses, breakdownForTooltip.Misses,
+                                breakdownForTooltip.Hits, breakdownForTooltip.CritHits),
+                            ProbabilityBar(
+                                0, rollDistances.Count - atLeastThatDistance,
+                                atLeastThatDistance, 0))
+                        .Replace(
+                            "vs. {b}15{/b}",
+                            $"vs. {{b}}{distanceToTarget * 5}{{/b}} feet")
+                        .Replace(
+                            "{b}15 DC",
+                            $"{{b}}{distanceToTarget * 5} DC")
+                        + $"\n{Math.Max(0, (distanceToTarget * 5) - 15).Greenify()} Adjusted distance";
+
+                    string ProbabilityBar(int fumbles, int fails, int hits, int crits)
+                    {
+                        return $"{{probabilitybar:check:{fumbles}:{fails}:{hits}:{crits}}}";
+                    }
+                });
+                
                 // Roll to Jump
-                leap.WithEffectOnChosenTargets(async (innerLeap, leaper, leapTargets) =>
+                //leap.EffectOnChosenTargets = null;
+                leap.EffectOnChosenTargets = async (innerLeap, leaper, leapTargets) =>
                 {
                     // Roll spec is added later so that it can be rolled without lost info.
                     // Breakdown is done manually to improve combat log printings.
-                    
-                    TaggedCalculatedNumberProducer jumpSkill = owner.HasFeat(ModData.FeatNames.GracefulLeaper)
-                        ? TaggedChecks.SkillCheck(Skill.Athletics, Skill.Acrobatics)
-                        : TaggedChecks.SkillCheck(Skill.Athletics);
                     CheckBreakdown breakdown = CombatActionExecution.BreakdownAttack(
-                        innerLeap
-                            .WithActiveRollSpecification(new ActiveRollSpecification(
-                                jumpSkill,
-                                new TaggedCalculatedNumberProducer((_, _, _) =>
-                                    new CalculatedNumber(15, "Long Jump DC", [])))),
+                        leap.WithActiveRollSpecification(new ActiveRollSpecification(
+                            jumpSkill,
+                            new TaggedCalculatedNumberProducer((_, _, _) =>
+                                new CalculatedNumber(15, "Long Jump DC", [])))),
                         caster);
                     CheckBreakdownResult result = new CheckBreakdownResult(breakdown);
                     
@@ -223,19 +284,20 @@ public static class LongJump
                     // Log the check result
                     string whatHappens = result.CheckResult switch
                     {
-                        CheckResult.CriticalFailure => "leaps normally (" + leapDistance*5 + " feet), then falls prone",
-                        CheckResult.Failure => "leaps normally (" + leapDistance*5 + " feet)",
-                        _ => "jumps " + leapDistance*5 + " feet",
+                        CheckResult.CriticalFailure => $"leaps normally ({leapDistance * 5} feet), then falls prone",
+                        CheckResult.Failure => $"leaps normally ({leapDistance * 5} feet)",
+                        _ => $"jumps {leapDistance * 5} feet",
                     } + ".";
                     leaper.Overhead(
                         result.CheckResult.HumanizeTitleCase2(),
                         Color.WhiteSmoke,
                         $"{leaper} gets a {result.CheckResult.Greenify()} and " + whatHappens,
                         innerLeap.Name,
-                        breakdown.DescribeWithFinalRollTotal(result)
-                            + (leaper.HasFeat(FeatName.PowerfulLeap) ? "\n\nHas Powerful Leap" : "\n")
-                            + $"\nSpeed: {leaper.Speed*5} feet"
-                            + $"\nFinal distance: {leapDistance*5} feet");
+                        $"""
+                          {breakdown.DescribeWithFinalRollTotal(result)}
+                          
+                          {ExplainJumpResult(leaper, leapDistance, result)}
+                          """);
                     
                     // Perform Leap
                     if (finalTile != null)
@@ -246,13 +308,15 @@ public static class LongJump
                         await leaper.FallProne();
 
                     // Die, if you're a superhero trying to get their mojo back by leaping across buildings.
-                    if (leaper.Occupies.Kind is TileKind.Chasm
+                    if (leaper.Space.Tiles.All(tile => tile.Kind is TileKind.Chasm)
                         && !leaper.HasEffect(QEffectId.Flying))
                     {
                         leaper.Battle.Log(leaper + " falls to their demise.");
                         leaper.Die();
                     }
-                });
+                };
+
+                #endregion
                 
                 // Execute, revert if you didn't Leap.
                 if (!await caster.Battle.GameLoop.FullCast(leap))
@@ -295,5 +359,28 @@ public static class LongJump
             self,
             20 + self.Skills.Get(Skill.Athletics),
             CheckResult.CriticalSuccess);
+    }
+
+    public static string ExplainJumpResult(Creature leaper, int finalLeapDistance, CheckBreakdownResult breakResult)
+    {
+        int normalLeapFeet = CalculateJumpDistanceResult(leaper, 0, CheckResult.Failure) * 5;
+        int finalLeapFeet = finalLeapDistance * 5;
+        int speedFeet = leaper.Speed * 5;
+        string jumpExplanation = 
+            $$"""
+              {b}{{finalLeapFeet}} feet breakdown:{/b}
+              {b}{{normalLeapFeet}}{b} Leap{{(leaper.HasEffect(QEffectId.PowerfulLeap) || leaper.HasEffect(QEffectId.PowerfulLeap2) ? " (Powerful)" : null)}}
+              """;
+        if (breakResult.CheckResult < CheckResult.Success)
+            jumpExplanation += $"\n{{b}}{{Red}}+0{{/Red}}{{/b}} Roll ({breakResult.CheckResult.ToStringOrTechnical().Capitalize()})";
+        else
+        {
+            int rolledFeet = breakResult.TotalRollValue / 5 * 5;
+            jumpExplanation += $"\n{(rolledFeet - normalLeapFeet).Greenify()} Roll (Nearest square)";
+            if (rolledFeet > speedFeet)
+                jumpExplanation += $"\n{(speedFeet - rolledFeet).Greenify()} Your speed";
+        }
+
+        return jumpExplanation;
     }
 }
